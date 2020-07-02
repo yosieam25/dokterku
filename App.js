03448074f774df5a0f35afdd80af161a850e318e
yosie abdul muzanil <yosie.am05@gmail.com>
2020-07-02 22:16:59 +0700

    part1

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {SplashScreen} from './src/pages';
 
 export default function App() {
--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Image} from 'react-native';
 import dokterku from '../../assets/logo/dokterku.png';
 
 export default function SplashScreen() {
   return (
     <View>
-      <Image source={dokterku} style={{width: 200, height: 200}} />
+      <Image source={dokterku} />
     </View>
   );
 }
